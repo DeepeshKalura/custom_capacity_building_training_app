764ace208af295db623236af2572b63c397d4e4f
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-07-10 17:12:18 +0530

    Whole File Changes
    1. Add Svg Package
    2. Added Categories in comment for future use
    3. SafeArea also intoduce
    4. AppBar 5. new body
    6. Bottom Navigaton bar still old

--- tranining_app/lib/view/screen/home/home_screen.dart
+++ tranining_app/lib/view/screen/home/home_screen.dart
@@ -1,89 +1,394 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  // final List<String> _categories = [
+  //   'All',
+  //   'Business',
+  //   'Design',
+  //   'Development',
+  //   'Finance',
+  //   'Health',
+  //   'IT & Software',
+  //   'Marketing',
+  //   'Music',
+  //   'Office Productivity',
+  //   'Personal Development',
+  //   'Photography',
+  //   'Teaching & Academics',
+  // ];
+
+  final _searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _searchController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          const Text('Catelog'),
-          const Text('Most Popular Certifications'),
-          Container(
-            height: 200,
-            constraints: const BoxConstraints(
-              maxWidth: 200 * 9,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color.fromRGBO(229, 238, 243, 1),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          bottom: PreferredSize(
+            preferredSize:
+                Size.fromHeight(MediaQuery.of(context).size.height * 0.06),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Hello,",
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                          children: [
+                            const TextSpan(
+                              text: 'Bruce Wayne  ',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontStyle: FontStyle.italic,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            TextSpan(
+                              text: String.fromCharCode(0x1F44B),
+                              style: const TextStyle(fontSize: 30),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CircleAvatar(
+                          radius: 24,
+                          backgroundColor: const Color(0xFFA06CF0),
+                          child: SvgPicture.asset(
+                            'assets/svg/female_avatar.svg',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: [
-                Container(
-                  width: 200,
-                  color: Colors.red,
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 12.0, left: 20.0, right: 12.0, bottom: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.72,
+                        // height: 50,
+                        child: TextField(
+                          controller: _searchController,
+                          onChanged: (value) {},
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: const BorderSide(
+                                color: Colors.transparent,
+                              ),
+                            ),
+                            hintText: 'Search',
+                            suffixIcon: IconButton(
+                              icon: const Icon(Icons.search),
+                              onPressed: () {
+                                _searchController.clear();
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFA06CF0),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.filter_list),
+                      ),
+                    ),
+                  ],
                 ),
-                Container(
-                  width: 200,
-                  color: Colors.blue,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  "Course",
+                  style: Theme.of(context).textTheme.displayLarge,
                 ),
-                Container(
-                  width: 200,
-                  color: Colors.green,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      elevation: MaterialStateProperty.all<double>(0.0),
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromRGBO(83, 192, 250, 1),
+                      ),
+                      fixedSize: MaterialStateProperty.all<Size>(
+                        const Size(180, 60),
+                      ),
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.black),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CircleAvatar(
+                          radius: 20,
+                          backgroundColor: Colors.white,
+                          child: Icon(
+                            Icons.play_arrow,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text("All Topics"),
+                      ],
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      elevation: MaterialStateProperty.all<double>(0.0),
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromRGBO(83, 192, 250, 1),
+                      ),
+                      fixedSize: MaterialStateProperty.all<Size>(
+                        const Size(180, 60),
+                      ),
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.black),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CircleAvatar(
+                          radius: 20,
+                          backgroundColor: Colors.white,
+                          child: Icon(
+                            Icons.play_arrow,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text("Popular"),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      elevation: MaterialStateProperty.all<double>(0.0),
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromRGBO(83, 192, 250, 1),
+                      ),
+                      fixedSize: MaterialStateProperty.all<Size>(
+                        const Size(180, 60),
+                      ),
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.black),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CircleAvatar(
+                          radius: 20,
+                          backgroundColor: Colors.white,
+                          child: Icon(
+                            Icons.play_arrow,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text("Newest"),
+                      ],
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      elevation: MaterialStateProperty.all<double>(0.0),
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromRGBO(83, 192, 250, 1),
+                      ),
+                      fixedSize: MaterialStateProperty.all<Size>(
+                        const Size(180, 60),
+                      ),
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.black),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CircleAvatar(
+                          radius: 20,
+                          backgroundColor: Colors.white,
+                          child: Icon(
+                            Icons.play_arrow,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text("Advance"),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height / 2.5,
+                width: MediaQuery.of(context).size.width * 0.6,
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(86, 183, 234, 1),
+                  borderRadius: BorderRadius.circular(20),
                 ),
-                Container(
-                  width: 200,
-                  color: Colors.yellow,
+                child: Column(
+                  children: [
+                    SvgPicture.asset(
+                      'assets/svg/female_avatar.svg',
+                      height: (MediaQuery.of(context).size.height / 3) * 0.4,
+                      width: MediaQuery.of(context).size.width * 0.6,
+                      fit: BoxFit.cover,
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'Title',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 5),
+                    const Text(
+                      'Description',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            SvgPicture.asset(
+                              'assets/svg/google.svg',
+                              height: 20,
+                              width: 20,
+                            ),
+                            const SizedBox(width: 5),
+                            const Text(
+                              '17',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            SvgPicture.asset(
+                              'assets/svg/facebook.svg',
+                              height: 20,
+                              width: 20,
+                            ),
+                            const SizedBox(width: 5),
+                            const Text(
+                              '2 hour',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-                Container(
-                  width: 200,
-                  color: Colors.orange,
-                ),
-                Container(
-                  width: 200,
-                  color: Colors.purple,
-                ),
-                Container(
-                  width: 200,
-                  color: Colors.pink,
-                ),
-                Container(
-                  width: 200,
-                  color: Colors.teal,
-                ),
-                Container(
-                  width: 200,
-                  color: Colors.brown,
-                ),
-              ],
+              ),
+            ],
+          ),
+        ),
+        // ! Pretty Messy Bottom Navigation Bar here
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 0,
+          onTap: (index) {},
+          fixedColor: Colors.red,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
             ),
-          ),
-          // ! Will implement this later
-        ],
-      ),
-      // ! Pretty Messy Bottom Navigation Bar here
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
-        onTap: (index) {},
-        fixedColor: Colors.red,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.bookmark),
-            label: 'Bookmark',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.bookmark),
+              label: 'Bookmark',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+        ),
       ),
     );
   }
