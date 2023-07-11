014117deeeb162cdab1a52e10dcd789b1413e87b
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-07-11 17:53:16 +0530

    Completed the signup page

--- tranining_app/lib/view/screen/auth/signup_screen.dart
+++ tranining_app/lib/view/screen/auth/signup_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -25,205 +26,266 @@ class _SignUpScreenState extends State<SignUpScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height * 0.4,
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 20, top: 10, bottom: 10),
-              child: Text("Sign up"),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ElevatedButton(
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                    ),
-                    side: MaterialStateProperty.all<BorderSide>(
-                      const BorderSide(
-                        color: Colors.black,
-                        width: 1,
-                      ),
-                    ),
-                    fixedSize: MaterialStateProperty.all<Size>(
-                      const Size(70, 60),
-                    ),
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                      Colors.transparent,
-                    ),
-                  ),
-                  onPressed: () {},
-                  child: const Text("Google"),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height * 0.40,
+                child: SvgPicture.asset(
+                  "assets/svg/signup.svg",
+                  fit: BoxFit.cover,
                 ),
-                ElevatedButton(
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                    ),
-                    side: MaterialStateProperty.all<BorderSide>(
-                      const BorderSide(
-                        color: Colors.black,
-                        width: 1,
-                      ),
-                    ),
-                    fixedSize: MaterialStateProperty.all<Size>(
-                      const Size(70, 60),
-                    ),
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                      Colors.transparent,
-                    ),
-                  ),
-                  onPressed: () {},
-                  child: const Text("Facebook"),
-                ),
-                ElevatedButton(
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                    ),
-                    side: MaterialStateProperty.all<BorderSide>(
-                      const BorderSide(
-                        color: Colors.black,
-                        width: 1,
-                      ),
-                    ),
-                    fixedSize: MaterialStateProperty.all<Size>(
-                      const Size(70, 60),
-                    ),
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                      Colors.transparent,
-                    ),
-                  ),
-                  onPressed: () {},
-                  child: const Text("Facebook"),
-                ),
-              ],
-            ),
-            const Center(
-              child: Padding(
-                padding: EdgeInsets.only(top: 15, bottom: 20),
-                child: Text("Or, Register With email"),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 20, top: 10, bottom: 10, right: 20),
-              child: Form(
-                key: _formKey,
-                child: Column(
+              Padding(
+                padding: const EdgeInsets.only(left: 20, top: 0, bottom: 10),
+                child: Text(
+                  "Sign up",
+                  style: Theme.of(context).textTheme.displayLarge,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  bottom: 5,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    TextFormField(
-                      controller: _nameController,
-                      decoration: const InputDecoration(
-                        prefixIcon: Icon(Icons.person),
-                        hintText: "Name",
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        side: MaterialStateProperty.all<BorderSide>(
+                          const BorderSide(
+                            color: Colors.grey,
+                            width: 1,
+                          ),
+                        ),
+                        fixedSize: MaterialStateProperty.all<Size>(
+                          const Size(100, 60),
+                        ),
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                          Colors.transparent,
+                        ),
+                        elevation: MaterialStateProperty.all<double>(0),
                       ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return "Please enter your name";
-                        }
-                        return null;
-                      },
+                      onPressed: () {},
+                      child: SvgPicture.asset(
+                        "assets/svg/google.svg",
+                        width: 40,
+                        height: 40,
+                      ),
                     ),
-                    TextFormField(
-                      controller: _emailController,
-                      decoration: const InputDecoration(
-                        prefixIcon: Icon(Icons.alternate_email),
-                        hintText: "Email ID",
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        elevation: MaterialStateProperty.all<double>(0),
+                        side: MaterialStateProperty.all<BorderSide>(
+                          const BorderSide(
+                            color: Colors.grey,
+                            width: 1,
+                          ),
+                        ),
+                        fixedSize: MaterialStateProperty.all<Size>(
+                          const Size(100, 60),
+                        ),
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                          Colors.transparent,
+                        ),
                       ),
-                      // TODO: Make a better validation
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return "Please enter your email";
-                        }
-                        return null;
-                      },
+                      onPressed: () {},
+                      child: SvgPicture.asset(
+                        "assets/svg/facebook.svg",
+                        width: 40,
+                        height: 40,
+                      ),
                     ),
-                    TextFormField(
-                      controller: _passwordController,
-                      decoration: const InputDecoration(
-                        prefixIcon: Icon(Icons.lock),
-                        hintText: "Password",
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        elevation: MaterialStateProperty.all<double>(0),
+                        side: MaterialStateProperty.all<BorderSide>(
+                          const BorderSide(
+                            color: Colors.grey,
+                            width: 1,
+                          ),
+                        ),
+                        fixedSize: MaterialStateProperty.all<Size>(
+                          const Size(100, 60),
+                        ),
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                          Colors.transparent,
+                        ),
                       ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return "Please enter your password";
-                        }
-                        return null;
-                      },
-                    ),
-                    TextFormField(
-                      controller: _confirmPasswordController,
-                      decoration: const InputDecoration(
-                        prefixIcon: Icon(Icons.lock),
-                        hintText: "Confirm Password",
+                      onPressed: () {},
+                      child: SvgPicture.asset(
+                        "assets/svg/microsoft.svg",
+                        width: 40,
+                        height: 40,
                       ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return "Please enter your password";
-                        }
-                        return null;
-                      },
                     ),
                   ],
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 20,
-                top: 10,
-                bottom: 10,
-                right: 20,
-              ),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  fixedSize: Size(MediaQuery.of(context).size.width * 0.9, 50),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 2,
                 ),
-                onPressed: () {},
-                child: const Text("Sign up"),
+                child: Row(
+                  children: [
+                    const Expanded(
+                      child: Divider(
+                        color: Colors.blueGrey,
+                        thickness: 1,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 2),
+                      child: Text(
+                        'OR',
+                        style: Theme.of(context).textTheme.labelLarge,
+                      ),
+                    ),
+                    const Expanded(
+                      child: Divider(
+                        color: Colors.blueGrey,
+                        thickness: 1,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 15, bottom: 20),
-                child: RichText(
-                  text: TextSpan(
-                    text: "Already have an account? ",
-                    style: const TextStyle(color: Colors.black),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 20, top: 4, bottom: 8, right: 20),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
                     children: [
-                      TextSpan(
-                        onEnter: (event) {
-                          // TOTO: Check it and also can be use as function
-                        },
-                        text: "Login",
-                        style: const TextStyle(
-                          color: Colors.blue,
-                          decoration: TextDecoration.underline,
+                      TextFormField(
+                        controller: _nameController,
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Icons.person),
+                          hintText: "Name",
                         ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Please enter your name";
+                          }
+                          return null;
+                        },
+                      ),
+                      TextFormField(
+                        controller: _emailController,
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Icons.alternate_email),
+                          hintText: "Email ID",
+                        ),
+                        // TODO: Make a better validation
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Please enter your email";
+                          }
+                          return null;
+                        },
+                      ),
+                      TextFormField(
+                        controller: _passwordController,
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Icons.lock),
+                          hintText: "Password",
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Please enter your password";
+                          }
+                          return null;
+                        },
+                      ),
+                      TextFormField(
+                        controller: _confirmPasswordController,
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Icons.lock),
+                          hintText: "Confirm Password",
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Please enter your password";
+                          }
+                          return null;
+                        },
                       ),
                     ],
                   ),
                 ),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 20,
+                  top: 2,
+                  bottom: 2,
+                  right: 20,
+                ),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      fixedSize:
+                          Size(MediaQuery.of(context).size.width * 0.9, 50),
+                      elevation: 0),
+                  onPressed: () {},
+                  child: const Text("Sign up"),
+                ),
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 15, bottom: 10),
+                  child: RichText(
+                    text: TextSpan(
+                      text: "Already have an account? ",
+                      style: Theme.of(context).textTheme.labelSmall?.copyWith(
+                            color: Colors.grey[700],
+                          ),
+                      children: [
+                        TextSpan(
+                          onEnter: (event) {
+                            // TOTO: Check it and also can be use as function
+                          },
+                          text: "Login",
+                          style:
+                              Theme.of(context).textTheme.labelSmall?.copyWith(
+                                    decoration: TextDecoration.underline,
+                                  ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
