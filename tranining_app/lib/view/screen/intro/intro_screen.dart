b8ee538caf0f5e1ada36eab89c74a8ce36302e93
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-07-11 06:36:38 +0530

    45 % introduction screen is completed

--- tranining_app/lib/view/screen/intro/intro_screen.dart
+++ tranining_app/lib/view/screen/intro/intro_screen.dart
@@ -1,1 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 
+import '../home/home_screen.dart';
+
+class IntroScreen extends StatefulWidget {
+  const IntroScreen({super.key});
+
+  @override
+  State<IntroScreen> createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen> {
+  final _introKey = GlobalKey<IntroductionScreenState>();
+
+  void _onIntroEnd(context) {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (_) => const HomeScreen()),
+    );
+  }
+
+  Widget _buildImage(String assetName, [double width = 350]) {
+    return Image.asset('assets/$assetName', width: width);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return IntroductionScreen(
+      showNextButton: false,
+      showDoneButton: true,
+      onDone: () => _onIntroEnd(context),
+      next: const Icon(Icons.arrow_forward),
+      done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
+      key: _introKey,
+      globalBackgroundColor: const Color.fromRGBO(229, 238, 243, 1),
+      globalHeader: Align(
+        alignment: Alignment.topRight,
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 16, right: 16),
+            child: TextButton(
+              onPressed: () => _onIntroEnd(context),
+              child: const Text(
+                'Skip',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      pages: [
+        PageViewModel(
+          title: 'Welcome to the app',
+          body: 'This is a description of the app',
+          image: _buildImage('illustration/login.jpg'),
+          decoration: const PageDecoration(
+            pageColor: Color.fromRGBO(229, 238, 243, 1),
+          ),
+        ),
+        PageViewModel(
+          title: 'Find Your Favorite Lesson',
+          body: 'This is a description of the app',
+          image: _buildImage('illustration/login.jpg'),
+          decoration: const PageDecoration(
+            pageColor: Color.fromRGBO(229, 238, 243, 1),
+          ),
+        ),
+        PageViewModel(
+          title: 'Welcome to the app',
+          body: 'This is a description of the app',
+          image: _buildImage('illustration/login.jpg'),
+          decoration: const PageDecoration(
+            pageColor: Color.fromRGBO(229, 238, 243, 1),
+          ),
+        ),
+        PageViewModel(
+          title: 'Welcome to the app',
+          body: 'This is a description of the app',
+          image: _buildImage('illustration/login.jpg'),
+          decoration: const PageDecoration(
+            pageColor: Color.fromRGBO(229, 238, 243, 1),
+          ),
+        ),
+      ],
+    );
+  }
+}
